c1d753d49b2fdc7dabaa5ad4a2216b0519364976
Tiago <thyaggus@gmail.com>
2022-09-12 09:07:29 -0300

    Update Performance.java

--- Performance.java
+++ Performance.java
@@ -16,39 +16,48 @@ public class Performance {
         start = System.currentTimeMillis();
     }
 
+    private static String stress(String s) {
+        String a = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
+        a += s.toUpperCase() + a;
+        s.replace("A", "b");
+        a = a.toUpperCase();
+        a = a.toLowerCase();
+        return a;
+    }
+
     private static void medirTempos() {
         List<String> strings = new ArrayList<>();
 
         startTime();
         for (int i = 0; i < 100000; i++) {
-            strings.add("aaaaaaaaaaaaa");
+            strings.add("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
         }
         stopTime(".......iniciacao do vetor");
 
         startTime();
         List<String> concats = new ArrayList<>();
         for (String s: strings) {
-            concats.add((s + "ssssssssss").replace("a", "b"));
+            concats.add(stress(s));
         }
         stopTime("......................for");
 
         startTime();
-        strings.stream().map(s -> (s + "ssssssssss").replace("a", "b")).collect(Collectors.toList());
+        strings.stream().map(s -> stress(s)).collect(Collectors.toList());
         stopTime("...................stream");
 
         startTime();
-        strings.parallelStream().map(s -> (s + "ssssssssss").replace("a", "b")).collect(Collectors.toList());
+        strings.parallelStream().map(s -> stress(s)).collect(Collectors.toList());
         stopTime("...........parallelStream");
 
         startTime();
-        strings.stream().parallel().map(s -> (s + "ssssssssss").replace("a", "b")).collect(Collectors.toList());
+        strings.stream().parallel().map(s -> stress(s)).collect(Collectors.toList());
         stopTime("......stream().parallel()");
 
         startTime();
-        strings.stream().map(s -> (s + "ssssssssss").replace("a", "b")).parallel().collect(Collectors.toList());
+        strings.stream().map(s -> stress(s)).parallel().collect(Collectors.toList());
         stopTime("stream().mpa().parallel()");
 
-        strings.
+        strings = null;
     }
 
     public static void main(String[] args) {
